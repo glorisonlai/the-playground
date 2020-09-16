8dd57fd7e9baed4b324b06ec24b759bcfb7f70b5
Glorison <lai.glorison@gmail.com>
2020-09-17 00:35:31 +1000

    finished bubs bg

--- client/src/app/index.ts
+++ client/src/app/index.ts
@@ -1,3 +1,14 @@
-import Background from './components/background/background';
+import Background from './components/backgrounds/bubbs/bubbles';
+
+console.log(
+'-------------------------------------------------------\n' +
+'|                                                     |\n' +
+'|              Bored of the background?               |\n' +
+'|               Try unlocking some! ;)                |\n' +
+'|                                                     |\n' +
+'-------------------------------------------------------'
+);
+
+
 
 export { Background };
