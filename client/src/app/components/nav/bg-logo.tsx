b309cb1c67a401e8be3acea34823ab9b42c96199
Glorison <lai.glorison@gmail.com>
2020-12-11 15:54:11 +1100

    Stuff

--- client/src/app/components/nav/bg-logo.tsx
+++ client/src/app/components/nav/bg-logo.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const BgLogo = (logoId) => {
-    const selectSvg = (logoId) => {
+const BgLogo = (logoId: Number) => {
+    const selectSvg = (logoId: Number) => {
     return (
-      <use xlink:href=`{/assets/img/sprites/${logoId}` />
+    //   <use xlink:href=`{/assets/img/sprites/${logoId}` />
+        <button onClick={() => {console.log('Open menu')}}/>
     )
   }
   return (
-      <button class="circle-btn glowy-btn" aria-label="menu">
-        <svg class="bg-sprite">
+      <button className="circle-btn glowy-btn" aria-label="menu">
+        <svg className="bg-sprite">
           {selectSvg(logoId)}
         </svg>
       </button>
   )
 }
+
+export default BgLogo;
--- client/src/app/components/nav/navbar.tsx
+++ client/src/app/components/nav/navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-      <ul class="nav" aria-label="menu">
+      <ul className="nav" aria-label="menu">
         <li><a className="nav-item">Home</a></li>
         <li><a className="nav-item">Experience</a></li>
         <li><a className="nav-item">Projects</a></li>
