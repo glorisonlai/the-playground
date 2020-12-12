8cb55b04fb778af7515719f138f6cd8ccef29595
Glorison <lai.glorison@gmail.com>
2020-12-13 03:51:09 +1100

    added conditional imports

--- client/src/components/backgrounds/background.tsx
+++ client/src/components/backgrounds/background.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import Bubbles from './bubbs/bubbles';
 import Lines from './lines/lines';
 
 const Background = ({bg}) => {
-  return <Lines/>;
+  const BubbleBg = lazy(() => import('./bubbs/bubbles'));
+  const LinesBg = lazy(() => import('./lines/lines'));
+  const bgs = {
+    0: <BubbleBg />,
+    1: <LinesBg />,
+  }
+
+  return (
+    <Suspense fallback={<div className="bg"></div>}>
+      { bgs[bg] }
+    </Suspense>
+  )
 }
 
 export default Background;
